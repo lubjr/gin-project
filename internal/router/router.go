a2be476114ba4a8138416e5b205a82b45a38d101
lubjr <luizeduardobjr@outlook.com>
2026-02-25 20:12:16 -0300

    feat: adds custom handlers for 404 and 405 errors

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,6 +9,16 @@ import (
 func Setup() *gin.Engine {
 	r := gin.Default()
 
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"error": "Route Not Found"})
+	})
+
+	r.HandleMethodNotAllowed = true
+
+	r.NoMethod(func(c *gin.Context) {
+		c.JSON(405, gin.H{"error": "Method Not Allowed"})
+	})
+
 	r.GET("/", handler.Healthy)
 	r.GET("/hello", handler.Hello)
 	r.POST("/calc", handler.Calc) 
